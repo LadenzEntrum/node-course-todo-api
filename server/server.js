489b2d90c2b43930e07653dcdb03e33b6ba1e9f2
TP <t@p.com>
2018-07-29 12:41:11 +0200

    DELETE route added

--- server/server.js
+++ server/server.js
@@ -46,16 +46,21 @@ app.get('/todos/:id', (req,res) => {
     }
     return res.send({todo});
   }).catch((e) => res.status(400).send());
+});
 
+app.delete('/todos/:id',(req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
 
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo){
+      return res.status(404).send();
+    }
+    res.send(todo);
 
-  //findById
-    //success
-      //if todo send it back
-      //if no todo dend back 404 with empty body
-    //error -> 400- send empty body back
-
-
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
